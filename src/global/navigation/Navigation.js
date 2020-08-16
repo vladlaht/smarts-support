4733cbf0d6d04a808232bfc857435abaa6956040
Vladislav Lahtarin <vlad.laht@gmail.com>
2020-08-16 14:06:31 +0300

    Updated navigation elements

--- src/global/navigation/Navigation.js
+++ src/global/navigation/Navigation.js
@@ -13,38 +13,44 @@ export default props => {
             <div className="sidebar-logo">
                 <img src={process.env.PUBLIC_URL + "/img/small_nav_logo.png"} alt="small-nav-logo"/>
             </div>
-            <div className={props.hasOwnProperty("activeRoute") && props.activeRoute === HOME_ROUTE ? "sidebar-link active" : "sidebar-link"}>
-                <Link className="sidebar-link-content" to="/">
-                    <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
-                        <FaHome/>
-                    </IconContext.Provider>
-                    Home
-                </Link>
-            </div>
-            <div className={props.hasOwnProperty("activeRoute") && props.activeRoute === TICKETS_ROUTE ? "sidebar-link active" : "sidebar-link"}>
-                <Link className="sidebar-link-content" to="/tickets">
-                    <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
-                        <FaTasks/>
-                    </IconContext.Provider>
-                    Tickets
-                </Link>
-            </div>
-            <div className={props.hasOwnProperty("activeRoute") && props.activeRoute === CLIENTS_ROUTE ? "sidebar-link active" : "sidebar-link"}>
-                <Link className="sidebar-link-content" to="/clients">
-                    <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
-                        <FaUsers/>
-                    </IconContext.Provider>
-                    Clients
-                </Link>
-            </div>
-            <div className={props.hasOwnProperty("activeRoute") && props.activeRoute === INVOICES_ROUTE ? "sidebar-link active" : "sidebar-link"}>
-                <Link className="sidebar-link-content" to="/invoices">
-                    <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
-                        <FaFileInvoiceDollar/>
-                    </IconContext.Provider>
-                    Invoices
-                </Link>
-            </div>
+            <Nav>
+                <div
+                    className={props.hasOwnProperty("activeRoute") && props.activeRoute === HOME_ROUTE ? "sidebar-link active" : "sidebar-link"}>
+                    <Link className="sidebar-link-content" to="/">
+                        <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
+                            <FaHome/>
+                        </IconContext.Provider>
+                        Home
+                    </Link>
+                </div>
+                <div
+                    className={props.hasOwnProperty("activeRoute") && props.activeRoute === TICKETS_ROUTE ? "sidebar-link active" : "sidebar-link"}>
+                    <Link className="sidebar-link-content" to="/tickets">
+                        <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
+                            <FaTasks/>
+                        </IconContext.Provider>
+                        Tickets
+                    </Link>
+                </div>
+                <div
+                    className={props.hasOwnProperty("activeRoute") && props.activeRoute === CLIENTS_ROUTE ? "sidebar-link active" : "sidebar-link"}>
+                    <Link className="sidebar-link-content" to="/clients">
+                        <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
+                            <FaUsers/>
+                        </IconContext.Provider>
+                        Clients
+                    </Link>
+                </div>
+                <div
+                    className={props.hasOwnProperty("activeRoute") && props.activeRoute === INVOICES_ROUTE ? "sidebar-link active" : "sidebar-link"}>
+                    <Link className="sidebar-link-content" to="/invoices">
+                        <IconContext.Provider value={{className: "sidebar-link-content__logo"}}>
+                            <FaFileInvoiceDollar/>
+                        </IconContext.Provider>
+                        Invoices
+                    </Link>
+                </div>
+            </Nav>
         </div>
     )
 }
